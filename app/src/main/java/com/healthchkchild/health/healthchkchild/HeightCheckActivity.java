b8758bdf7f847a430ecb95db876052fed7bdc704
ekdk636 <ekdk636@gmail.com>
2017-09-22 17:04:15 +0900

    20170922_layout

---
+++ app/src/main/java/com/healthchkchild/health/healthchkchild/HeightCheckActivity.java
@@ -0,0 +1,13 @@
+package com.healthchkchild.health.healthchkchild;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeightCheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_height_check);
+    }
+}
---
+++ app/src/main/java/com/healthchkchild/health/healthchkchild/UserRegInActivity.java
@@ -0,0 +1,13 @@
+package com.healthchkchild.health.healthchkchild;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserRegInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_reg_in);
+    }
+}
